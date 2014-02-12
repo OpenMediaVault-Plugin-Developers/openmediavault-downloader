1d8b273f9d4a70011d0483d99d32f9849cb85e14
Aaron Murray <aaron@omv-extras.org>
2014-02-12 07:24:03 -0600

    update for kralizec

--- var/www/openmediavault/js/omv/module/admin/service/downloader/Upload.js
+++ var/www/openmediavault/js/omv/module/admin/service/downloader/Upload.js
@@ -1,5 +1,10 @@
 /**
- * Copyright (C) 2013-2014 OpenMediaVault Plugin Developers
+ *
+ * @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @author    Volker Theile <volker.theile@openmediavault.org>
+ * @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+ * @copyright Copyright (c) 2009-2013 Volker Theile
+ * @copyright Copyright (c) 2013-2014 OpenMediaVault Plugin Developers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
